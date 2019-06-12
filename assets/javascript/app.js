eb40ac2ac2943d5620285754af7dbc45a8371c1f
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-06-11 19:33:37 -0700

    Gifs now pause upon click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -84,13 +84,32 @@ function renderButtons() {
         
         var gifImage = $("<img>");
         gifImage.attr("src", results[i].images.fixed_height.url);
-        
+        gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+        gifImage.attr("data-animate", results[i].images.fixed_height.url);
+        gifImage.attr("data-state", "still");
+        gifImage.addClass("gif");
         gifDiv.prepend(p);
         gifDiv.prepend(gifImage);
         
         $("#gifs-appear-here").prepend(gifDiv);
       }
       console.log("index 4",pastryOpt[4])
+// this bit of code taken from an activity should load in the gifs as still and then animate them 
+//on a click
+      $(".gif").on("click", function() {
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("data-state");
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
     });
   });
+});
   };
